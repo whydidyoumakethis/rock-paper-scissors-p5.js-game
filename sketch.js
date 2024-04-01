9e44b7641d67a7d48be90927e37235e96e940b0c
saud abu khodair <whydidyoumakethis@gmail.com>
2024-04-01 04:03:26 +0300

    made "win :D" text dynamic

--- sketch.js
+++ sketch.js
@@ -93,8 +93,9 @@ for(i=0; i<types.length; i++) {
     }
   }
   if(allmatch) {
-    textSize(50);
+    
     rect(width/4, height/4, width/2, height/2);
+    textSize(floor((windowWidth+windowHeight)/20));
     text(types[i] + " wins :D", width/2, height/2);
     return true;
   }
